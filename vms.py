ce964db528ac62a518e149bc232e123145a210ec
Phil Adams <pad@stac.school.nz>
2018-06-29 15:06:56 +1200

    can now rent vehicles
    Can rent vehicles by entering the licence plate number. Updated summary of all vehicles is then printed.

--- vms.py
+++ vms.py
@@ -1,14 +1,31 @@
 # vehicle management system for a rental company
-# vehicles have names, seats and availability, renters
+# vehicles have names, seats availability and renters
 
 class Vehicle:
 
-    def __init__(self, name, seats):
+    def __init__(self, name, seats, licence):
         self._name = name
         self._seats = seats
+        self._licence = licence
         self._renter = ""
         self._available = True
         vehicles.append(self)
+
+    def _display_info_available(self):
+        print("============")
+        print(self._name)
+        print("Seats: {}".format(self._seats))
+        print("Licence: {}".format(self._licence))
+
+    def _display_all(self):
+        print("===============")
+        print(self._name)
+        print("Seats: {}".format(self._seats))
+        print("Licence: {}".format(self._licence))
+        if self._available == True:
+            print("Available")
+        else:
+            print("Currently unavailable. Renter name: {}".format(self._renter))
 
     def _hire(self, renter):
         self._available = False
@@ -22,18 +39,37 @@ class Vehicle:
 vehicles = []
 
 # create vehicles
-Vehicle("Subaru Outback", 5)
-Vehicle("Ford Escape", 5)
-Vehicle("Toyota Hiace", 8)
+Vehicle("Subaru Outback", 5, "ABC123")
+Vehicle("Ford Escape", 5, "TYG321")
+Vehicle("Toyota Hiace", 8, "AAB3")
 
-# display all vehicles
-def view_all():
-    for v in vehicles:
-        print("{}, ({})".format(v._name, v._seats))
+
 
 # display available based on seats
-def available():
-    pass
+def rent_vehicle():
+    print("Enter number of seats required:")
+    while True:
+        try:
+            seat_search = int(input("Seats: "))
+            break
+        except:
+            print("Enter an integer")
+    for v in vehicles:
+        if v._seats >= seat_search and v._available ==True:
+            v._display_info_available()
+    
+    rent_out = input("Enter licence of vehicle to rent: ")
+    renter_name = input("Enter renter name:")
+    counter = 0
+    for v in vehicles:
+        if v._licence == rent_out:
+            v._hire(renter_name)
+            counter +=1
+    if counter == 0:
+        print("Licence plate incorrect. No vehicle rented")
+            
 
-view_all()
-
+rent_vehicle()
+for v in vehicles:
+        
+    v._display_all()
